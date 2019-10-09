dfe3f9ac4222ff5aee6e946d80da52a2a04a1d13
Сашка <aleksandrdergelev@mail.ru>
2019-10-09 18:37:40 +0300

    Initial Commit

---
+++ FocusStartTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FocusStartTestTask
+//
+//  Created by Александр Дергилёв on 09/10/2019.
+//  Copyright © 2019 Александр Дергилёв. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
